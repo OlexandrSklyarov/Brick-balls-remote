d37578d56eb493f70b191bb94ca6bb48c53b858b
OlexandrSklyarov <alexskrat@gmail.com>
2019-07-25 21:09:34 +0300

    Added GPGS 0.9.53

--- Assets/GooglePlayGames/ISocialPlatform/PlayGamesPlatform.cs
+++ Assets/GooglePlayGames/ISocialPlatform/PlayGamesPlatform.cs
@@ -759,6 +759,10 @@ namespace GooglePlayGames
                 {
                     mClient.IncrementAchievement(achievementID, numSteps, callback);
                 }
+                else if (callback != null)
+                {
+                    callback.Invoke(false);
+                }
             }
             else if (progress >= 100)
             {
@@ -772,6 +776,10 @@ namespace GooglePlayGames
                 // not enough to unlock
                 GooglePlayGames.OurUtils.Logger.d("Progress " + progress +
                     " not enough to unlock non-incremental achievement.");
+                if (callback != null)
+                {
+                    callback.Invoke(false);
+                }
             }
         }
 
@@ -1183,7 +1191,7 @@ namespace GooglePlayGames
         /// Shows the standard Google Play Games leaderboards user interface,
         /// which allows the player to browse their leaderboards. If you have
         /// configured a specific leaderboard as the default through a call to
-        /// <see cref="SetDefaultLeaderboardForUi" />, the UI will show that
+        /// <see cref="SetDefaultLeaderboardForUI" />, the UI will show that
         /// specific leaderboard only. Otherwise, a list of all the leaderboards
         /// will be shown.
         /// </summary>
